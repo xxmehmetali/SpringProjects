34298cf39fdbfe042f5d4b01c0736c80cd352fa2
Mehmet Ali Karadağ <xxmehmetali@hotmail.com>
2023-03-19 00:54:45 +0300

    update

--- L2p2-wiring-autowiring-qualifier/src/main/java/beans/Owner.java
+++ L2p2-wiring-autowiring-qualifier/src/main/java/beans/Owner.java
@@ -1,24 +1,47 @@
 package beans;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Owner {
 
-    private Cat cat;
+//    this is a way of injecting cat object. But we generally make fields final. So it would cause problem.
+//    @Autowired
+//    private Cat cat;
+
+//    fix of the problem is following as in constructor:
+    private final Cat cat;
+
+    //putting a constructor will solve the problem.
+    @Autowired
+    public Owner(final Cat cat){
+        this.cat = cat;
+    }
+
+    //Qualifier and Autowired needed together to inject bean.
+    @Qualifier("dogAlice")
+    @Autowired
+    private Dog dog;
+
+//------------------------------------
+
 
     public Cat getCat() {
         return cat;
     }
 
-    public void setCat(Cat cat) {
-        this.cat = cat;
-    }
+//    public void setCat(Cat cat) {
+//        this.cat = cat;
+//    }
+
 
     @Override
     public String toString() {
         return "Owner{" +
                 "cat=" + cat +
+                ", dog=" + dog +
                 '}';
     }
 }
--- L2p2-wiring-autowiring-qualifier/src/main/java/config/ProjectConfig.java
+++ L2p2-wiring-autowiring-qualifier/src/main/java/config/ProjectConfig.java
@@ -1,7 +1,9 @@
 package config;
 
 import beans.Cat;
+import beans.Dog;
 import beans.Owner;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,4 +12,24 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 @ComponentScan(basePackages = "beans")
 public class ProjectConfig {
+
+    // Note that there are 2 Dog bean without annotated with @Primary. But @Qualifier provides consumers to call beans with specified name.
+    // It is like bean id. You can call it with @Qualifier annotation from somewhere else again.
+
+    @Bean
+    @Qualifier("dogAlice")
+    public Dog dog1(){
+        Dog dog = new Dog();
+        dog.setName("dog alice");
+        return dog;
+    }
+
+    @Bean
+    @Qualifier("dogBob")
+    public Dog dog2(){
+        Dog dog = new Dog();
+        dog.setName("dog bob");
+        return dog;
+    }
+
 }
